1a531682f90538f0ca0bcce1845cca9a7bb5036f
PetarSimonovic <petar@simonovic.com>
2021-12-29 11:31:27 +0000

    Initial Commit

---
+++ SlowFrame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlowFrame
+//
+//  Created by Petar Simonovic on 29/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SlowFrame/SlowFrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlowFrameApp.swift
+//  SlowFrame
+//
+//  Created by Petar Simonovic on 29/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct SlowFrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
