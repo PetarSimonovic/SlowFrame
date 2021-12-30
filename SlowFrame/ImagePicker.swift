8110d4209daaf94506e3111e4d8a61dd3da1ff96
PetarSimonovic <petar@simonovic.com>
2021-12-30 15:01:10 +0000

    image picker created: brings up user's photo library

--- SlowFrame/ImagePicker.swift
+++ SlowFrame/ImagePicker.swift
@@ -3,22 +3,30 @@
 //  SlowFrame
 //
 //  Created by Petar Simonovic on 29/12/2021.
-//
-//
-//import Foundation
-//
-//import PhotosUI
-//import SwiftUI
-//
-//struct CustomView: UIViewControllerRepresentable {
-//    
-//    func makeUIView(context: Context) -> UITextView {
-//        return UITextView()
-//    }
-//    
-//    func updateUIView(_ uiView: UITextView, context: Context) {
-//     
-//            // Update the view
-//        }
-//    
-//}
+
+
+import Foundation
+
+import PhotosUI
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        
+        let picker = PHPickerViewController(configuration: config)
+        return picker
+        
+    }
+    
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+        
+    }
+    
+    
+    typealias UIViewControllerType = PHPickerViewController
+
+    
+}
