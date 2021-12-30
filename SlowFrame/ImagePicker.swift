58640a441cfeb6f86e96a5320af2ff8125932df0
PetarSimonovic <petar@simonovic.com>
2021-12-30 14:20:53 +0000

    Can display an image from Assets

---
+++ SlowFrame/ImagePicker.swift
@@ -0,0 +1,24 @@
+//
+//  ImagePicker.swift
+//  SlowFrame
+//
+//  Created by Petar Simonovic on 29/12/2021.
+//
+//
+//import Foundation
+//
+//import PhotosUI
+//import SwiftUI
+//
+//struct CustomView: UIViewControllerRepresentable {
+//    
+//    func makeUIView(context: Context) -> UITextView {
+//        return UITextView()
+//    }
+//    
+//    func updateUIView(_ uiView: UITextView, context: Context) {
+//     
+//            // Update the view
+//        }
+//    
+//}
